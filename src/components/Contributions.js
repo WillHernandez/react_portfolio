0cf55ad8bb922ce1c3c6e1f28df6012dbdbae9b7
Will Hernandez <w.hernandez.code@gmail.com>
2021-07-06 11:14:58 -0400

    commit 2 added, extra images removed

--- src/components/Contributions.js
+++ src/components/Contributions.js
@@ -11,6 +11,7 @@ const Contributions = () => {
         <ContribComponent title={data.five.title} issue={data.five.issue} href={data.five.href} src={data.five.img} desc={data.five.desc}/>
         <ContribComponent title={data.three.title} issue={data.three.issue} href={data.three.href} src={data.three.img} desc={data.three.desc}/>
         <ContribComponent title={data.one.title} issue={data.one.issue} href={data.one.href} src={data.one.img} desc={data.one.desc}/>
+        <ContribComponent title={data.two.title} issue={data.two.issue} href={data.two.href} src={data.two.img} desc={data.two.desc}/>
         <ContribComponent title={data.four.title} issue={data.four.issue} href={data.four.href} src={data.four.img} desc={data.four.desc}/>
         <Link to='/contributions'>MORE CONTRIBUTIONS</Link>
       </div>
@@ -54,10 +55,11 @@ const data = {
     desc: 'Fixed the order in which a visited pages mimeType was set. Added a new mimeType to recognize and allow the formatting of minified asset files when directly visited on a webpage.'
   },
   two: {
-    title: 'Devtools incorrectly opens a new browser window when refreshed.',
-    href: 'https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2606131/',
-    img: '/img/2606131.jpg',
-    desc: 'WIP'
+    title: 'URL.createObjectURL() creates valid links.',
+    issue: 'https://bugs.chromium.org/p/chromium/issues/detail?id=1100124',
+    href: 'https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2991382/',
+    img: '/img/2991383.jpg',
+    desc: 'URL.createObjectURL() creates a clickable link that does not include \'blob\' which is a required for correct webpage routing. If url is type \'blob\', include all sections when creating a clickable link.'
   },
   three: {
     title: 'Created a check on our document for any custom css varibles.',
@@ -79,5 +81,5 @@ const data = {
     href: 'https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2909881',
     img: '/img/2909881.jpg',
     desc: 'Created a JSON parser that allows .json format network responses to be Pretty Printed. Enabled Pretty Print button on .json file types. Wrote Mocha tests to confirm parser worked as expected.'
-  }
+  },
 };
