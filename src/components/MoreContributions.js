9fe176d7b3d0935e7367441c5c4e950399ebc3b1
Will Hernandez <w.hernandez.code@gmail.com>
2021-07-27 10:32:07 -0400

    small patch added to morecontributions

--- src/components/MoreContributions.js
+++ src/components/MoreContributions.js
@@ -12,6 +12,7 @@ const MoreContributions = () => {
         <div className="returnHome">
           <Link to='/'>RETURN HOME</Link>
         </div>
+        <Contribution link={data.three.link} href={data.three.href} desc={data.three.desc} />
         <Contribution link={data.one.link} href={data.one.href} desc={data.one.desc} />
         <Contribution link={data.two.link} href={data.two.href} desc={data.two.desc} />
       </div>
@@ -37,13 +38,18 @@ const Contribution = (props) => {
 }
 
 const data = {
+  three: {
+    link: 'https://bugs.chromium.org/p/chromium/issues/detail?id=998674&q=component%3APlatform%3EDevTools%20json&can=2&start=100',
+    href: 'https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3055035/',
+    desc:"Corrected a previous patch I had submitted. JSON formatter now respects the user's indentation level provided in Chrome Settings > Preference > Sources section > Default indentation",
+  },
   one: {
     link: 'https://bugs.chromium.org/p/chromium/issues/detail?id=1137733',
     href: 'https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2493185',
     desc:'Prevent “Emulated Devices” text from wrapping. Changed a style property on the “Emulated Devices” h1 to prevent it from wrapping to the next line and clashing with the "Add custom device” button when the DevTools window is resized.',
   },
   two: {
-    link: 'https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2606131/',
+    link: 'https://bugs.chromium.org/p/chromium/issues/detail?id=1070605',
     href: 'https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2606131/',
     desc: 'Work In Progress.',
   },
